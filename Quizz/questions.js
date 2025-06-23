e363a61b31ee4c462e0686bede9141b0636d09bd
Margot Gouriou <margot.gouriou@gmail.com>
2025-06-23 15:22:46 +0200

    questions.js

---
+++ Quizz/questions.js
@@ -0,0 +1,70 @@
+export const quizz_film = {
+  questions: [
+    {
+      text: "Capitaine, oh mon capitaine !",
+      options: [
+        "Pirates des Caraïbes.",
+        "Le cercle des poètes disparus.",
+        "Peter Pan.",
+        "Trainspotting."
+      ],
+      correct_answer: "Le cercle des poètes disparus."
+    },
+    {
+      text: "Ils peuvent prendre nos vies, mais ils ne prendront jamais notre liberté !",
+      options: [
+        "Troie.",
+        "300.",
+        "Braveheart",
+        "Gladiator."
+      ],
+      correct_answer: "Braveheart."
+    },
+
+ {
+      text: "La cuillère n'existe pas",
+      options: [
+        "Matrix.",
+        "Blade runner.",
+        "Le crime de l'Orient Express",
+        "L'aile ou la cuisse."
+      ],
+      correct_answer: "Matrix."
+    },
+
+    {
+      text: "Tu te souviens de cette fois, dans ta tête, où tu t’es dit : C’est une mauvaise idée ? Tu aurais dû écouter.",
+      options: [
+        "Fight Club.",
+        "Joker.",
+        "Mad Max fury road",
+        "Batman."
+      ],
+      correct_answer: "Mad Max fury road."
+    },
+
+ {
+      text: "J'ai une arme. Je vais tirer. Si tu ne pars pas, je tirerai.",
+      options: [
+        "Il était une fois dans l'Ouest.",
+        "Casino.",
+        "Les Affranchis",
+        "Enter the void."
+      ],
+      correct_answer: "Enter the void."
+    },
+
+ {
+      text: "Hey Mickey ! Tu veux aller au pipiroom ?",
+      options: [
+        "Snatch.",
+        "Les Goonies.",
+        "Spy Kids",
+        "Mickey."
+      ],
+      correct_answer: "Les Goonies."
+    },
+
+    // Autres questions...
+  ]
+};
