0dd21e7080d8e3b6d35f59a2c977f4d190694ea1
Elouan <elouangauriaud@gmail.com>
2025-06-25 14:07:50 +0200

    Implémentation d'un score en fin de partie

--- Quizz/game.js
+++ Quizz/game.js
@@ -4,8 +4,10 @@ const questionElement = document.getElementById('question-text');
 const optionsContainer = document.getElementById('options-container');
 const nextButton = document.getElementById('next-button'); // Ajoute un bouton dans le fichier HTML
 const replayButton = document.getElementById('replay-button'); // Ajoute un bouton rejouer dans le fichier HTML
-let currentQuestionIndex = 0;
+let currentQuestionIndex = 0; // Prends le début de l'index des questions au démarrage du quizz.
+let score = 0; // Met le score à 0 au start.
 
+replayButton.style.display = 'none';
 function loadQuestion() {
   nextButton.disabled = true; // Désactive le bouton suivant au début de chaque question
   const currentQuestion = quizz_film.questions[currentQuestionIndex];
@@ -30,7 +32,8 @@ nextButton.addEventListener('click', () => {
   if (currentQuestionIndex < quizz_film.questions.length) {
     loadQuestion();
   } else {
-    questionElement.innerText = "C'est fini, merci d'avoir participé !";
+    // Affiche le message de fin avec le score.
+    questionElement.innerText = `C'est fini, merci d'avoir participé ! Ton score total sur ce quizz est de : ${score} / ${quizz_film.questions.length}.`;
     optionsContainer.innerHTML = '';
     nextButton.style.display = 'none'; // Désactive le bouton Suivant
     replayButton.style.display = 'inline-block'; // Afficher le bouton Rejouer
@@ -57,22 +60,26 @@ function checkAnswer(clickedButton, correctAnswer) {
     } else {
       button.classList.add('incorrect');
     }
+});
+  // Augmente le score de +1 à chaque bonne réponse, pas d'actions si mauvaise réponse.
+    if (normalizeText(clickedButton.innerText) === normalizeText(correctAnswer)) {
+      score++;
+    }
   // Permet de réactiver le bouton suivant lorsque la réponse est cliquée.
   nextButton.disabled = false;
-  })
 };
   // Fonction pour réinitialiser le quizz
   replayButton.addEventListener('click', () => {
   // Réinitialiser l'index 
   currentQuestionIndex = 0;
+  score = 0; //Réinitialise le score au redémarrage.
 
   replayButton.style.display = 'none';
   nextButton.style.display = 'inline-block';
 
-  // Recharger la première questionAdd commentMore actions
+  // Recharger la première question
   loadQuestion();
   });
-
-
+  
 loadQuestion();
 
